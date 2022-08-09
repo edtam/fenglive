b6419919e1f07cd96c93e9bf2d118f8cf5cffa50
edtam <414689070@qq.com>
2022-08-09 20:33:32 +0800

    修改统计加载

---
+++ src/js/stats.js
@@ -0,0 +1,12 @@
+/**
+ * 百度统计
+ */
+if (import.meta.env.PROD) {
+  var _hmt = _hmt || []
+  ;(function () {
+    var hm = document.createElement('script')
+    hm.src = 'https://hm.baidu.com/hm.js?40e3c7d069ba3632a3479d7d3adb9fba'
+    var s = document.getElementsByTagName('script')[0]
+    s.parentNode.insertBefore(hm, s)
+  })()
+}
